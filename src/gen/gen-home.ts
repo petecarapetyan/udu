67c41b8385560e59c0189bda2369052aa43e4018
petecarapetyan <pete@datafundamentals.com>
2021-03-18 16:29:54 -0500

    WIP

--- src/gen/gen-home.ts
+++ src/gen/gen-home.ts
@@ -33,34 +33,34 @@ export const genCallToAction = (genLoremSpec: GenLoremSpec) => {
 export const genTeaserSection = (genLoremSpec: GenLoremSpec) => {
   console.log(genLoremSpec.topMenuCount)
   return `teasertitle: Why Rocket?
-  feature:
-    - title: Small
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: No overblown tools or frontend frameworks, add JavaScript and/or Web Components only on pages where needed.
-  teasers:
-    - title: Small
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: No overblown tools or frontend frameworks, add JavaScript and/or Web Components only on pages where needed.
-    - title: Pre-Rendered
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: Statically generated content means less javascript to ship and process.
-    - title: Zero Configuration
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: Automatic code splitting, filesystem based routing, and javascript in markdown.
-    - title: Meta Framework
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: 'Build on top of giants like <a href="https://www.11ty.dev/">eleventy</a>, <a href="https://rollupjs.org/">Rollup</a>, and <a href="https://www.modern-web.dev/">Modern Web</a>.'
-    - title: Powerful Default Template
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: Provide content and you are ready to go.
-    - title: Ready for Production
-      href: /blah
-      img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
-      text: Optimized for a smaller build size, faster dev compilation and dozens of other improvements.`
+feature:
+  - title: Small
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: No overblown tools or frontend frameworks, add JavaScript and/or Web Components only on pages where needed.
+teasers:
+  - title: Small
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: No overblown tools or frontend frameworks, add JavaScript and/or Web Components only on pages where needed.
+  - title: Pre-Rendered
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: Statically generated content means less javascript to ship and process.
+  - title: Zero Configuration
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: Automatic code splitting, filesystem based routing, and javascript in markdown.
+  - title: Meta Framework
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: 'Build on top of giants like <a href="https://www.11ty.dev/">eleventy</a>, <a href="https://rollupjs.org/">Rollup</a>, and <a href="https://www.modern-web.dev/">Modern Web</a>.'
+  - title: Powerful Default Template
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: Provide content and you are ready to go.
+  - title: Ready for Production
+    href: /blah
+    img: <img class="bordered" src="/_merged_assets/_static/images/bulksplash-bagasvg-7VS__QB2vo4.jpg" alt="bulksplash-bagasvg-7VS__QB2vo4.jpg" />
+    text: Optimized for a smaller build size, faster dev compilation and dozens of other improvements.`
 }
